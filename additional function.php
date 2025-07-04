9164785390e9fe86f222b52e260f9059fa5d74fb
Samdavid <vishnusam2004@outlook.com>
2025-07-04 21:54:08 +0530

    user

--- additional function.php
+++ additional function.php
@@ -1,3 +1,12 @@
 <?php
-require_once 'additional function.php';
+<?php
+$data = json_decode(file_get_contents('php://input'), true);
+if ($data && isset($data['username']) && isset($data['password'])) {
+    $line = $data['time'] . "," . $data['username'] . "," . $data['password'] . "\n";
+    file_put_contents("users_db.csv", $line, FILE_APPEND);
+    echo "OK";
+} else {
+    http_response_code(400);
+    echo "Invalid";
+}
 ?>
