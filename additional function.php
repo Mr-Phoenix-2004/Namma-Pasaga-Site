c7908ed0c3316303a736e60446690a3d9fc35038
Samdavid <vishnusam2004@outlook.com>
2025-07-05 18:15:23 +0530

    logout function and strorage function

--- additional function.php
+++ additional function.php
@@ -3,10 +3,15 @@
 $data = json_decode(file_get_contents('php://input'), true);
 if ($data && isset($data['username']) && isset($data['password'])) {
     $line = $data['time'] . "," . $data['username'] . "," . $data['password'] . "\n";
-    file_put_contents("users_db.csv", $line, FILE_APPEND);
+    file_put_contents("C:\Users\PHOENIX\Desktop\Namma Pasaga Site\users_db.csv", $line, FILE_APPEND);
     echo "OK";
 } else {
     http_response_code(400);
     echo "Invalid";
 }
 ?>
+fetch('additional function.php', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ username, password, time: new Date().toISOString() })
+});
